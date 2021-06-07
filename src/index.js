5d772ffd905f49e7e07c955d1c356658841ddf2d
Bill Moore <ksbillymoore@gmail.com>
2021-06-07 16:27:51 -0500

    added lazy loading to pages in index, added loading spinner component

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import ReactDOM from "react-dom";
 //import { createBrowserHistory } from "history";
 import { HashRouter, Route, Switch } from "react-router-dom";
@@ -8,20 +8,31 @@ import "assets/scss/material-kit-react.scss?v=1.9.0";
 
 // pages for this product
 import Components from "views/Components/Components.js";
-import ProjectsPage from "views/ProjectsPage/ProjectsPage.js";
-import ProfilePage from "views/ProfilePage/ProfilePage.js";
-import LoginPage from "views/LoginPage/LoginPage.js";
+//import ProjectsPage from "views/ProjectsPage/ProjectsPage.js";
+// import ProfilePage from "views/ProfilePage/ProfilePage.js";
+// import LoginPage from "views/LoginPage/LoginPage.js";
+
+import LoadingSpinner from "components/LoadingSpinner/LoadingSpinner";
+const ProjectsPage = lazy(() => import('views/ProjectsPage/ProjectsPage.js') )
+const ProfilePage = lazy(() => import('views/ProfilePage/ProfilePage.js') )
+const LoginPage = lazy(() => import('views/LoginPage/LoginPage.js') )
 
 //var hist = createBrowserHistory();
 
 ReactDOM.render(
   <HashRouter>
-    <Switch>
-      <Route path="/projects-page" component={ProjectsPage} />
-      <Route path="/profile-page" component={ProfilePage} />
-      <Route path="/login-page" component={LoginPage} />
-      <Route path="/" component={Components} />
-    </Switch>
+    <Suspense fallback={
+      <div style={{display: 'Grid', justifyContent: 'center', alignContent: 'center', }}>
+        <LoadingSpinner />
+      </div>
+    }>
+      <Switch>
+          <Route path="/projects-page" component={ProjectsPage} />
+          <Route path="/profile-page" component={ProfilePage} />
+          <Route path="/login-page" component={LoginPage} />
+          <Route path="/" component={Components} />
+      </Switch>
+      </Suspense>
   </HashRouter>,
   document.getElementById("root")
 );
