51e272f433419fac2364495131370a3f6acc9220
Bill Moore <ksbillymoore@gmail.com>
2021-11-06 13:01:12 -0500

    Updates

---
+++ precache-manifest.9cfb391e45c85f1e5003a0189572497a.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b66db057cfa3d446131529cb84413b9b",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "aa9183057cd64e5e023d",
+    "url": "/portfolio/static/css/main.a1c5fe97.chunk.css"
+  },
+  {
+    "revision": "69e63a6cbeaa807ceb91",
+    "url": "/portfolio/static/js/2.7e9569e0.chunk.js"
+  },
+  {
+    "revision": "d09ac505fec0e9130a1d5b85fb3fa6dc",
+    "url": "/portfolio/static/js/2.7e9569e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83b6000d8c9a29ab3c33",
+    "url": "/portfolio/static/js/3.ec43a939.chunk.js"
+  },
+  {
+    "revision": "c358fb6efd23ee79bb92",
+    "url": "/portfolio/static/js/4.b003ac73.chunk.js"
+  },
+  {
+    "revision": "cb8d5815263c5f4a98c9",
+    "url": "/portfolio/static/js/5.2a93deca.chunk.js"
+  },
+  {
+    "revision": "215e1802a67b0942292b",
+    "url": "/portfolio/static/js/6.acc34ed9.chunk.js"
+  },
+  {
+    "revision": "aa9183057cd64e5e023d",
+    "url": "/portfolio/static/js/main.ceb77d45.chunk.js"
+  },
+  {
+    "revision": "c251be4e9842592093e0098333b51410",
+    "url": "/portfolio/static/js/main.ceb77d45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bc5bc08456bb7dba807",
+    "url": "/portfolio/static/js/runtime-main.de9bbd17.js"
+  },
+  {
+    "revision": "2f5aa7f15a5b55e3f63879e540753bab",
+    "url": "/portfolio/static/media/Bill Moore Resume.2f5aa7f1.pdf"
+  },
+  {
+    "revision": "f755b80a990cdef957e0d8c9c623725e",
+    "url": "/portfolio/static/media/Player-queue.f755b80a.jpg"
+  },
+  {
+    "revision": "cd87277ed206d13053576a0076fc8016",
+    "url": "/portfolio/static/media/TreasureHunt.cd87277e.jpg"
+  },
+  {
+    "revision": "b08234a2b7e8e4fb2e3c4e713252021c",
+    "url": "/portfolio/static/media/bg7.b08234a2.jpg"
+  },
+  {
+    "revision": "44a795155fe8c5b8fa9510c425c62124",
+    "url": "/portfolio/static/media/bg9.44a79515.jpg"
+  },
+  {
+    "revision": "fc389ff99346e2a56fdfc89aa7e4cf83",
+    "url": "/portfolio/static/media/bootstrap-logo.fc389ff9.png"
+  },
+  {
+    "revision": "970b93f0f37f1af926ba32a3200c0879",
+    "url": "/portfolio/static/media/css-logo.970b93f0.png"
+  },
+  {
+    "revision": "c521e7e67222276a6860665a46813cc6",
+    "url": "/portfolio/static/media/firebase-logo.c521e7e6.png"
+  },
+  {
+    "revision": "4494093c2b0abb831c256ddec7d4f920",
+    "url": "/portfolio/static/media/forest-dark.4494093c.jpg"
+  },
+  {
+    "revision": "6c8f0a0e377cfa70efcd35ff52893b17",
+    "url": "/portfolio/static/media/html-logo.6c8f0a0e.png"
+  },
+  {
+    "revision": "0e4fdce8ac22e09688c580e5bc4dcd7d",
+    "url": "/portfolio/static/media/javaScript-logo.0e4fdce8.png"
+  },
+  {
+    "revision": "4d69bd33df8bf20ad5b84d192a88adde",
+    "url": "/portfolio/static/media/material-logo.4d69bd33.png"
+  },
+  {
+    "revision": "e2df515215a2035392aac9e23829908b",
+    "url": "/portfolio/static/media/profile-bg2.e2df5152.jpg"
+  },
+  {
+    "revision": "f3913006f47c355afa84da090f6c41df",
+    "url": "/portfolio/static/media/profile-pic-Me.f3913006.jpg"
+  },
+  {
+    "revision": "f81a6f373c244b1f70f4b7402b5ab372",
+    "url": "/portfolio/static/media/react-logo.f81a6f37.png"
+  },
+  {
+    "revision": "c8f44df3b0bdbe3addb48058ed44c088",
+    "url": "/portfolio/static/media/redux-logo.c8f44df3.png"
+  },
+  {
+    "revision": "14ce879b081c67c0faf107ec8f8af9e2",
+    "url": "/portfolio/static/media/sass-logo.14ce879b.png"
+  },
+  {
+    "revision": "cf8b686b294041d0925f4e745b1fabb9",
+    "url": "/portfolio/static/media/sign.cf8b686b.jpg"
+  },
+  {
+    "revision": "bfc388bea715be71d28e73e60ef8a5f5",
+    "url": "/portfolio/static/media/streamCounter.bfc388be.jpg"
+  }
+]);
