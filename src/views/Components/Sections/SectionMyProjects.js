79de50bb5d1101408c2489d2cbe967722cb262a3
Bill Moore <ksbillymoore@gmail.com>
2022-02-17 16:45:32 -0600

    added minor updates to home page and projects page for library content

--- src/views/Components/Sections/SectionMyProjects.js
+++ src/views/Components/Sections/SectionMyProjects.js
@@ -3,7 +3,7 @@ import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import { Typography } from '@material-ui/core'
 // react components for routing our app without refresh
-
+import { Link } from 'react-router-dom'
 // core components
 import GridContainer from "components/Grid/GridContainer.js";
 import GridItem from "components/Grid/GridItem.js";
@@ -11,6 +11,8 @@ import Button from "components/CustomButtons/Button.js";
 import treasure from "assets/img/TreasureHunt.jpg";
 import streamCounter from 'assets/img/streamCounter.jpg'
 import playerQueue from 'assets/img/Player-queue.jpg'
+import LibrarySet from 'assets/img/LibraryDouble.png'
+
 import styles from "assets/jss/material-kit-react/views/componentsSections/exampleStyle.js";
 
 const useStyles = makeStyles(styles);
@@ -23,13 +25,33 @@ export default function SectionLogin() {
         <GridContainer justify="center" spacing={2}>
           <GridItem xs={12}>
             <Typography variant='h2'>My Projects</Typography>
+            <Link to='projects-page'><Typography variant='h6'>Read about each one here</Typography></Link>
           </ GridItem>
 
           <GridItem xs={12} sm={12} md={6}>
             <a href='https://billy-moore.github.io/treasure-game/' target='_blank' rel="noopener noreferrer" className={classes.link}>
                 <img
+                  src={LibrarySet}
+                  alt="library projects"
+                  className={
+                    classes.imgRaised +
+                    " " +
+                    classes.imgRounded +
+                    " " +
+                    classes.imgFluid
+                  }
+                />
+                <Button color="white" size="lg" simple>
+                  Projects for the library
+                </Button>
+              </a>
+          </GridItem>
+
+          <GridItem xs={12} sm={12} md={6}>
+            <a href='https://billy-moore.github.io/treasure-game/' target='_blank' rel="noopener noreferrer" className={classes.link}>
+                <img
                   src={treasure}
-                  alt="..."
+                  alt="treasure game"
                   className={
                     classes.imgRaised +
                     " " +
@@ -48,7 +70,7 @@ export default function SectionLogin() {
             <a href='https://billy-moore.github.io/stream-counter' target='_blank' rel="noopener noreferrer" className={classes.link}>
               <img
                 src={streamCounter}
-                alt="..."
+                alt="stream counter"
                 className={
                   classes.imgRaised +
                   " " +
@@ -67,7 +89,7 @@ export default function SectionLogin() {
             <a href='https://billy-moore.github.io/player-queue/' target='_blank' rel="noopener noreferrer" className={classes.link}>
                 <img
                   src={playerQueue}
-                  alt="..."
+                  alt="player queue"
                   className={
                     classes.imgRaised +
                     " " +
--- src/views/ProjectsPage/Sections/ProductSection.js
+++ src/views/ProjectsPage/Sections/ProductSection.js
@@ -7,6 +7,7 @@ import GridItem from "components/Grid/GridItem.js";
 import treasure from 'assets/img/TreasureHunt.jpg'
 import streamCounter from 'assets/img/streamCounter.jpg'
 import playerQ from 'assets/img/Player-queue.jpg'
+import combo from 'assets/img/comboShot.png'
 
 import styles from "assets/jss/material-kit-react/views/landingPageSections/productStyle.js";
 
@@ -30,11 +31,47 @@ export default function ProductSection() {
       </GridContainer> */}
       <div className={classes.container}>
         <GridContainer spacing={3} >
+
+        <GridItem xs={12} sm={4} >
+                <img
+                  src={combo}
+                  alt="library and book list"
+                  style={{width:'100%', borderRadius: '10px'}}
+                />
+                 <h3 style={{color: 'black'}}>Library Projects</h3>
+                {/* <a href='https://billy-moore.github.io/treasure-game/' target='_blank' rel="noopener noreferrer" className={classes.link}> */}
+               
+                {/* </a> */}
+
+                {/* <a href='https://billy-moore.github.io/treasure-game/' target='_blank' rel="noopener noreferrer" className={classes.link}>
+                <h3>BookList Generator</h3>
+                </a> */}
+          </GridItem>
+          <GridItem xs={12} sm={8} container style={{color: 'black'}}>
+            <h3>Library Home Page & Book List generator</h3>
+                <p>
+                  These projects are very real results of being interested in programming while being a librarian during the day. I took over the library website when I became a librarian, and with only a basic understanding of HTML and CSS, I taught myself how to use bootstrap
+                  and work in the framework of the Libguides structure which is what the library uses to host its site. You can build a perfectly functioning and usable library website with great features, without knowing any code. But then your website looks like every other academic
+                  library website. I designed all the major aspects and used the University's prescibed color palette to make something that was united with the University's marketing materials, but still completely ours. It had to be funcitonal, easy to understand and navigate, dynamic,
+                  and maintanable. <br />
+                  The Book List Generator was a request from our acquisitions librarian. She needed some way to create a visual book list, so she could send an informational email to the entire campus. I said "I think I can make you something." This very basic application takes the Google Books
+                  REST API and allows you to input a book's ISBN number, and choose the correct book cover for the item you selected. It then allows you to modify a few options, delete selections, and then export the document to be saved as a PDF. I put this together in a couple days, error tested
+                  it, we've only used it a couple months and saw an instant increase in usage, inquiries and positive feedback. Plus it cut her production time down to minutes, as opposed to hours, which she was spending copy/pasting images from Amazon.
+                </p>
+                <GridItem xs={6}>
+                  <a href='https://cune.libguides.com/home' target='_blank' rel='noopener noreferrer' className={classes.link}>Library Homepage</a>
+                </GridItem>
+                  <GridItem xs={6}>
+                  <a href='https://billy-moore.github.io/book-list/' target='_blank' rel='noopener noreferrer' className={classes.link}>Book List Generator</a>
+
+                  </GridItem>
+          </GridItem>
+
           <GridItem xs={12} sm={4} >
           <a href='https://billy-moore.github.io/treasure-game/' target='_blank' rel="noopener noreferrer" className={classes.link}>
                 <img
                   src={treasure}
-                  alt="..."
+                  alt="treasure game"
                   style={{width:'100%', borderRadius: '10px'}}
                 />
                 <h3>Treasure Game</h3>
@@ -42,10 +79,11 @@ export default function ProductSection() {
           </GridItem>
           <GridItem xs={12} sm={8} style={{color: 'black'}}>
             <h3>Treasure of the Sergio Monco</h3>
-                <p>I really wanted to make something to prove I had learned something in React. I came up with the 
-                  mechanics for the game in about 4 minutes, but then I kept adding ideas. I created the tiles and icons 
+                <p>The title is a play on Spaghetti Westerns and Treasure of the Sierra Madre. I really wanted to make something to prove I had learned something in React. I came up with the 
+                  mechanics for the game quickly as a push-your-luck game where your high score is tied to your cash on hand. I created everything you see on the screen, the icons, shovels, everything but a very light 8-bit button CSS library. I created the tiles and icons 
                   using <a href='https://www.pixilart.com' alt='...' target='_blank' rel="noopener noreferrer">pixilart.com</a> and then just kept cleaning up code and 
-                  adding as I learned. Built around Context API, useReducer and a fun randomization algorithm.
+                  adding as I learned. This was a chance to use and build around React's Context API, learn how to use useReducer and a build around a fun randomization algorithm. It's a fully functioning game that has some really cool features and functionality, while looking really
+                  nice and tells a fun story.
                 </p>
           </GridItem>
           
@@ -53,7 +91,7 @@ export default function ProductSection() {
           <a href='https://billy-moore.github.io/stream-counter/' target='_blank' rel="noopener noreferrer" className={classes.link}>
                 <img
                   src={streamCounter}
-                  alt="..."
+                  alt="stream counter"
                   style={{width:'100%', borderRadius: '10px', border: 'solid 2px #e4e4e4'}}
                 />
                 <h3>Stream Counter</h3>
@@ -72,7 +110,7 @@ export default function ProductSection() {
           <a href='https://github.com/billy-moore/player-queue.git' target='_blank' rel="noopener noreferrer" className={classes.link}>
                 <img
                   src={playerQ}
-                  alt="..."
+                  alt="player queue"
                   style={{width:'100%', borderRadius: '10px', border: 'solid 2px #e4e4e4'}}
                 />
                 <h3>Player Queue App</h3>
